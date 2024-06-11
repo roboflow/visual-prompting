4cc447cae4376aebd36fbc90f7cec9d6ffbd0306
Ben Brooks <ben@directai.io>
2024-06-11 16:16:02 -0400

    build modification

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,22 +1,31 @@
-print("hello world")
+
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 
 app = FastAPI()
+@app.on_event("startup")
+async def startup_event():
+    app.config_dict = {}
+
+class Box(BaseModel):
+    class_: str
+    bbox: List[int]
 
 class Image(BaseModel):
-    image: str
+    contents: str
+    boxes: List[Box]
 
 @app.post("/train")
 async def train(images: List[Image]):
+    print(images)
     # You can now access your images with the "images" variable
     # Do something with the images here
     return {"message": "Images received!"}
 
 @app.post("/infer")
-async def train(model_id: str):
+async def infer(model_id: str):
     # You can now access your images with the "images" variable
     # Do something with the images here
     return {"message": "running inference!"}
